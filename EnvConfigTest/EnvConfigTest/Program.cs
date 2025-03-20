f03fbe53b137259cf1f49e3ca650d160f6da2a98
Flupinochan <flupino@metalmental.net>
2025-03-20 17:42:08 +0900

    add FrameResizer

---
+++ EnvConfigTest/EnvConfigTest/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Reflection;
+using System.Text.Json;
+
+namespace EnvConfigTest;
+
+public static class JsonOptionsProvider
+{
+    public static readonly JsonSerializerOptions Options = new JsonSerializerOptions { WriteIndented = true };
+}
+
+class Program
+{
+    /// <summary>
+    /// エントリーポイント
+    /// </summary>
+    static void Main()
+    {
+        try
+        {
+            // 埋め込みリソースからの設定を取得
+            Person embeddingConfig = GetEmbeddingResourceData();
+            Console.WriteLine();
+            // 外部ファイルからの設定を取得
+            Person? overrideConfig = GetOverrideData();
+            Console.WriteLine();
+            // 最終的な設定を取得
+            Person finalConfig = overrideConfig ?? embeddingConfig;
+            Console.WriteLine("最終的に使用される設定:");
+            Console.WriteLine(finalConfig);
+        }
+        catch(JsonException ex)
+        {
+            Console.WriteLine($"JSONデシリアライズエラー: {ex}");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"予期しないエラー: {ex}");
+        }
+    }
+
+
+    /// <summary>
+    /// 埋め込みリソースからデフォルト設定を取得する
+    /// </summary>
+    private static Person GetEmbeddingResourceData()
+    {
+        String[] embeddingResourceNames = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+        Console.WriteLine($"埋め込みリソース名一覧: {String.Join(Environment.NewLine, embeddingResourceNames)}");
+
+        String envResourceName = "EnvConfigTest.env.json";
+        Stream resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(envResourceName)
+            ?? throw new InvalidOperationException($"埋め込みリソース {envResourceName} が見つかりません");
+
+        using(resourceStream)
+        {
+            Person defaultConfig = JsonSerializer.Deserialize<Person>(resourceStream, JsonOptionsProvider.Options)
+                ?? throw new InvalidOperationException("埋め込みリソースのデータのデシリアライズに失敗しました");
+
+            Console.WriteLine("埋め込みリソースから取得したデータ:");
+            Console.WriteLine(defaultConfig);
+            return defaultConfig;
+        }
+    }
+
+
+    /// <summary>
+    /// 外部ファイルから設定を取得する
+    /// </summary>
+    private static Person? GetOverrideData()
+    {
+        String exeFolderPath = AppDomain.CurrentDomain.BaseDirectory;
+        Console.WriteLine($"exeフォルダのパス: {exeFolderPath}");
+
+        String envPath = Path.Combine(exeFolderPath, "env.json");
+        Console.WriteLine($"env.jsonのパス: {envPath}");
+
+        if(File.Exists(envPath))
+        {
+            Stream overrideStream = File.OpenRead(envPath);
+            using(overrideStream)
+            {
+                Person overrideConfig = JsonSerializer.Deserialize<Person>(overrideStream, JsonOptionsProvider.Options)
+                    ?? throw new InvalidOperationException("外部ファイルのデータのデシリアライズに失敗しました");
+
+                Console.WriteLine("外部ファイルから取得したデータ:");
+                Console.WriteLine(overrideConfig);
+                return overrideConfig;
+            }
+        }
+        else
+        {
+            Console.WriteLine($"外部ファイル {envPath} が見つかりません");
+            return null;
+        }
+    }
+}
+
+
+class Person
+{
+    public String Name { get; set; } = String.Empty;
+    public Int32 Age { get; set; } = -1;
+    public String Email { get; set; } = String.Empty;
+
+    public override String ToString() => JsonSerializer.Serialize(this, JsonOptionsProvider.Options);
+}
---
+++ FrameResizer/FrameResizer/View/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+using System.Windows;
+
+namespace FrameResizer;
+
+public partial class MainWindow : Window
+{
+    // ソースフォルダ名
+    private String _sourceFolderPath = String.Empty;
+    // 出力フォルダ名 (デフォルトはソースフォルダ名と同じ)
+    private String _outputFolderPath = String.Empty;
+    // 画像ファイル名
+    private List<String> _selectedImageNameList = new List<String>();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+
+    /// <summary>
+    /// File選択Button処理 フォルダ名およびファイル名を取得
+    /// </summary>
+    private void SelectFileButton_Click(Object sender, RoutedEventArgs e)
+    {
+        CommonOpenFileDialog fileDialog = new CommonOpenFileDialog
+        {
+            IsFolderPicker = false, // ファイル選択モード
+            Multiselect = true,     // 複数選択を許可
+            Title = "ファイルを選択してください ※複数選択可",
+        };
+        fileDialog.Filters.Add(new CommonFileDialogFilter("Images", "*.jpg;*.jpeg;*.png"));
+        fileDialog.Filters.Add(new CommonFileDialogFilter("All", "*.*"));
+
+        CommonFileDialogResult fileDialogResult = fileDialog.ShowDialog();
+        if(fileDialogResult == CommonFileDialogResult.Cancel || fileDialogResult == CommonFileDialogResult.None) return;
+        if(fileDialog.FileNames is null) return;
+
+        _sourceFolderPath = Path.GetDirectoryName(fileDialog.FileNames.First())!;
+        _outputFolderPath = Path.GetDirectoryName(fileDialog.FileNames.First())!;
+        _selectedImageNameList = fileDialog.FileNames.Select(filePath => Path.GetFileName(filePath)).ToList();
+
+        this.SelectedFileListBox.ItemsSource = _selectedImageNameList;
+        this.OutputFolderTextBox.Text = _outputFolderPath;
+    }
+
+    /// <summary>
+    /// Folder選択Button フォルダ名およびファイル名を取得
+    /// </summary>
+    private void SelectFolderButton_Click(Object sender, RoutedEventArgs e)
+    {
+        CommonOpenFileDialog folderDialog = new CommonOpenFileDialog
+        {
+            IsFolderPicker = true,
+            Multiselect = false,
+            Title = "フォルダを選択してください"
+        };
+
+        CommonFileDialogResult fileDialogResult = folderDialog.ShowDialog();
+        if(fileDialogResult == CommonFileDialogResult.Cancel || fileDialogResult == CommonFileDialogResult.None) return;
+        if(folderDialog.FileNames is null) return;
+
+        String[] imageExtensions = { ".jpg", ".jpeg", ".png" };
+        String[] selectedFilesPath = Directory.GetFiles(folderDialog.FileName)
+                                              .Where(filePath => imageExtensions.Contains(Path.GetExtension(filePath)))
+                                              .ToArray();
+        _sourceFolderPath = folderDialog.FileName;
+        _outputFolderPath = folderDialog.FileName;
+        _selectedImageNameList = selectedFilesPath.Select(filePath => Path.GetFileName(filePath)).ToList();
+
+        this.SelectedFileListBox.ItemsSource = _selectedImageNameList;
+        this.OutputFolderTextBox.Text = _outputFolderPath;
+    }
+
+    /// <summary>
+    /// Width RadioButton
+    /// </summary>
+    private void WidthRadioButton_Checked(Object sender, RoutedEventArgs e)
+    {
+        if (this.WidthDecimalUpDown is null || this.HeightDecimalUpDown is null) return;
+        this.WidthDecimalUpDown.IsEnabled = true;
+        this.HeightDecimalUpDown.IsEnabled = false;
+        this.HeightDecimalUpDown.Value = 0;
+    }
+
+    /// <summary>
+    /// Height RadioButton
+    /// </summary>
+    private void HeightRadioButton_Checked(Object sender, RoutedEventArgs e)
+    {
+        if (this.WidthDecimalUpDown is null || this.HeightDecimalUpDown is null) return;
+        this.HeightDecimalUpDown.IsEnabled = true;
+        this.WidthDecimalUpDown.IsEnabled = false;
+        this.WidthDecimalUpDown.Value = 0;
+    }
+}
